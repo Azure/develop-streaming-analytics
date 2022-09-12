2c00fb1e005c22a3c85c764d93b1899c9af2dae8
james-tn <james.nguyen@microsoft.com>
2022-09-12 11:06:07 -0700

    add simulation client

--- common/client/README.md
+++ common/client/README.md
@@ -1,1 +1,11 @@
 This folder is to develop client simulator applications including the app that generate events and the app that receives real time streaming computation from Cloud
+For simulator module. The simulator module create ride request and demand messages to EvenHub.
+To run the simulator:
+- Create EH namespace and 2 evenhubs, each with 4 partitions
+    1. Request_EH
+    2. Supply_EH
+- Generate a connection string and set the value into the environment variable, e.g.
+set EH_CONN="Endpoint=sb://XX.servicebus.windows.net/;SharedAccessKeyName=new;SharedAccessKey=XX/XX/XX="
+- Run the simulation program
+```python simul.py
+
---
+++ common/client/simul.py
@@ -0,0 +1,96 @@
+import asyncio
+import datetime
+import time
+import pandas as pd
+
+from azure.eventhub.aio import EventHubProducerClient
+from azure.eventhub import EventData
+import random
+import json
+import argparse
+import os
+def parse_args():
+    # arg parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--duration", default=100, type=int, help="duration to run the simulator")
+    parser.add_argument("--avail_duration", default=5, type=int, help="duration for car to appear as available in a location from start time")
+
+    parser.add_argument("--request_eh", type=str, default="ride_requests")
+    parser.add_argument("--supply_eh", type=str, default="car_availability")
+    # parse args
+    args = parser.parse_args()
+
+    # return args
+    return args
+
+
+locations = ["loc_"+str(i) for i in range(50)]
+riders = ["r_"+str(i) for i in range(100000)]
+cars = ["c_"+str(i) for i in range(1000)]
+types = ["x", "xl", "green", "comfort"]
+con_str = os.getenv('EH_CONN')
+async def send_ride_request_batch(producer):
+    # Without specifying partition_id or partition_key
+    # the events will be distributed to available partitions via round-robin.
+    for request_type in random.choices(types,k=len(types)):
+        event_batch = await producer.create_batch(partition_key=request_type)
+        for from_location in random.choices(locations,k=len(locations))  :            
+            for to_location in random.choices(locations,k=len(locations)):
+                timestamp = str(datetime.datetime.now())
+                record = json.dumps({"timestamp":timestamp, "from_loc":from_location, "to_loc":to_location, "request_type":request_type})
+                event_batch.add(EventData(record))
+        await producer.send_batch(event_batch)
+async def send_car_availability_batch(producer,avail_duration):
+    # Without specifying partition_id or partition_key
+    # the events will be distributed to available partitions via round-robin.
+    for car_type in random.choices(types,k=len(types)):
+        event_batch = await producer.create_batch(partition_key=car_type)
+        for location in random.choices(locations,k=len(locations)):
+            for _ in range(len(locations))  :            
+                starttime = datetime.datetime.now()
+                endtime = starttime+ datetime.timedelta(avail_duration)
+                car = random.choice(cars)
+                record = json.dumps({"starttime":str(starttime),"endtime":str(endtime), "location":location, "car_type":car_type, "car_id":car})
+            event_batch.add(EventData(record))
+        await producer.send_batch(event_batch)
+
+async def send_ride_requests(eh_name="ride_requests"):
+    producer = EventHubProducerClient.from_connection_string(conn_str=con_str, eventhub_name=eh_name)
+    async with producer:
+        await send_ride_request_batch(producer)
+async def send_car_availability(avail_duration, eh_name="car_availability"):
+    producer = EventHubProducerClient.from_connection_string(conn_str=con_str, eventhub_name=eh_name)
+    async with producer:
+        await send_car_availability_batch(producer,avail_duration)
+async def main(avail_duration,request_eh,supply_eh ):
+    task1 = asyncio.create_task(send_ride_requests(request_eh))
+
+    task2 = asyncio.create_task(send_car_availability(avail_duration,supply_eh))
+
+    print(f"started at {time.strftime('%X')}")
+
+    # Wait until both tasks are completed (should take
+    # around 2 seconds.)
+    await task1
+    await task2
+
+    print(f"finished at {time.strftime('%X')}")
+
+# # run script
+if __name__ == "__main__":
+    args = parse_args()
+    duration = args.duration
+    avail_duration = args.avail_duration
+    request_eh = args.request_eh
+    supply_eh = args.request_eh
+    execution_time=0
+    batch =0
+    while execution_time <duration:
+        batch += 1
+        start_time = time.time()
+        asyncio.run(main(avail_duration,request_eh,supply_eh ))
+
+        batch_time = time.time() - start_time
+        execution_time += batch_time
+        print("Batch {0}, sent {1} messages in {2} seconds.".format(batch, len(locations)*len(locations)*len(types), batch_time))
+    # run main function
